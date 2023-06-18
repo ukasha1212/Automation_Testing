e30822934619f1222b2565f1b6e0c80c2b8dda17
Ahmed Ukasha <ukasha@Ahmeds-MBP.lan>
2023-06-19 02:41:44 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+directory = "/Users/ukasha/Desktop/jib"
+images = []
+count = 0
+
+# Get the list of image files in the directory
+for img in os.listdir(directory):
+    if img.lower().endswith('.jpg'):
+        images.append(img)
+        count += 1
+
+# Rename and move the images to a new folder
+for i, img in enumerate(images):
+    old_filepath = os.path.join(directory, img)
+    new_filename = f"image_{i + 1:06d}.jpg"
+    new_directory= os.path.join(directory,"Image_" + str(i + 1))
+    os.makedirs(new_directory)
+    new_filepath = os.path.join(new_directory, new_filename)
+
+    shutil.move(old_filepath, new_filepath)
