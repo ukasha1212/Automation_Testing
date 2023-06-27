3ba1d0d78da0c2753b8d45da89ccaeda95b60c8e
ukasha ahmed <53560259+ukasha1212@users.noreply.github.com>
2023-06-27 13:53:13 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import os
 import shutil
+import re
 
-# Taking directory path as an input from user.
-directory = input("Enter the directory path") 
-
+directory = input("Enter the directory path :")
+sub_dir =[]
 images = []
 count = 0
+
+
+for item in os.listdir(directory):
+    if os.path.isdir(os.path.join(directory,item)):
+        sub_dir.append(item)
+
+pattern = r'Image_(\d)'
+for folder in sub_dir:
+    match = re.match(pattern, folder)
+    if match:
+        series = int(match.group(1))
+        if series > count:
+            count = series
+
+
+count = count+1
 
 # Get the list of image files in the directory
 for img in os.listdir(directory):
     if img.lower().endswith('.jpg'):
         images.append(img)
-        count += 1
 
 # Rename and move the images to a new folder
 for i, img in enumerate(images):
     old_filepath = os.path.join(directory, img)
-    new_filename = f"image_{i + 1:06d}.jpg"
-    new_directory= os.path.join(directory,"Image_" + str(i + 1))
-    os.makedirs(new_directory)
+    new_filename = f"image_{count + i :06d}.jpg"
+    new_directory = os.path.join(directory,"Image_" + str(count + i))
+    os.makedirs(new_directory, exist_ok=True)
     new_filepath = os.path.join(new_directory, new_filename)
-
     shutil.move(old_filepath, new_filepath)
