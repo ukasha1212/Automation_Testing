3be3ce440226b75b516102755e4f141d75531404
ukasha ahmed <53560259+ukasha1212@users.noreply.github.com>
2023-06-28 15:09:14 +1000

    Update main.py
    Updated using functions

--- main.py
+++ main.py
@@ -2,43 +2,54 @@ import os
 import shutil
 import re
 
-directory = input("Enter the directory path :")
-sub_dir =[]
-images = []
-count = 0
+def list_pattern_folders(directory):
+    sub_dir = []
+    pattern = r'Image_(\d)'
+    for folder in os.listdir(directory):
+        if os.path.isdir(os.path.join(directory,folder)):
+            match = re.match(pattern, folder)
+            if match:
+                sub_dir.append(folder)
+    return sub_dir
 
-#get list of item in directory
+def fetch_series (sub_dir):
+    pattern = r'Image_(\d)'
+    count =0
+    for folder in sub_dir:
+        match = re.match(pattern,folder)
+        if match:
+            series = int(match.group(1))
+            if series > count:
+                count = series
+    return count
 
-for item in os.listdir(directory):
-    if os.path.isdir(os.path.join(directory,item)):
-        sub_dir.append(item)
-
-#Defining search pattern and finding last number in naming convention of folder
-
-pattern = r'Image_(\d)'
-for folder in sub_dir:
-    match = re.match(pattern, folder)
-    if match:
-        series = int(match.group(1))
-        if series > count:
-            count = series
-
-#incrementng counter for next number in series for naming
-
-count = count+1
-
-# Get the list of 'jpg' files in the directory
-
-for img in os.listdir(directory):
-    if img.lower().endswith('.jpg'):
-        images.append(img)
+def moving_renamed_images_to_new_folder(directory, count):
+     images = []
+# Get the list of image files in the directory
+    for img in os.listdir(directory        if img.lower().endswith('.jpg'):
+            images.append(img)
 
 # Rename and move the images to a new folder
+    for i, img in enumerate(images):
+       old_filepath = os.path.join(directory, img)
+       new_filename = f"image_{count + i :06d}.jpg"
+       new_directory = os.path.join(directory,"Image_" + str(count + i))
+       os.makedirs(new_directory)
+       new_filepath = os.path.join(new_directory, new_filename)
+       shutil.move(old_filepath, new_filepath)
 
-for i, img in enumerate(images):
-    old_filepath = os.path.join(directory, img)
-    new_filename = f"image_{count + i :06d}.jpg"
-    new_directory = os.path.join(directory,"Image_" + str(count + i))
-    os.makedirs(new_directory)
-    new_filepath = os.path.join(new_directory, new_filename)
-    shutil.move(old_filepath, new_filepath)
+def main():
+
+
+    print('Enter the path of your directory example: user\macintosh\desktop')
+    directory = input("Enter the directory path :")
+    sub_dir = list_pattern_folders(directory)
+
+    count= fetch_series (sub_dir)
+
+    count= count+1
+
+    moving_renamed_images_to_new_folder(directory, count)
+
+if __name__ == "__main__":
+    main()
